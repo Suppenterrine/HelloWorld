662d082624c9195b25e0a764cde4e6a7ced6d02a
Lukas <lukas.baumert@hsb-elektro.de>
2016-10-25 15:42:45 +0200

    Der Benutzer wird zweimal aufgefordert eine Zahl einzugeben. Die eingegebene Zahl wird in den Datentyp "int" konvertiert und in einer Variable gespeichert (z.B. "iEingabeEins").
    Die beiden Variablen werden zusammenaddiert. Das Ergebnis wird in einer weiteren Variable (iErgebnis) gespeichert.
    Zum Schluss wird ein kurzer Satz und das Ergebnis angezeigt.

---
+++ ConvertToInt/ConvertToInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geben Sie eine Zahl ein: ");
+            int iEingabeEins = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Geben Sie noch eine Zahl ein: ");
+            int iEingabeZwei = Convert.ToInt16(Console.ReadLine());
+
+            int iErgebnis = iEingabeEins + iEingabeZwei;
+            Console.Write("Das Ergebnis der Addition: " + iErgebnis);
+
+            Console.ReadKey();
+        }
+    }
+}
